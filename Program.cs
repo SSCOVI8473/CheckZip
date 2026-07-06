3ba43680f9cc72257a1ba0ece77ed01faa6f9b23
Steven Scoville <SSCOVI8473@my.sullivan.edu>
2026-07-05 22:27:49 -0500

    feat: complete Part 1 zip code sorting

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] zipCodes = new string[10];
+
+Console.WriteLine("PLEASE ENTER 10 ZIP CODES");
+
+for (int i = 0; i < zipCodes.Length; i++)
+{
+    Console.Write("Zip code " + (i + 1) + ": ");
+    zipCodes[i] = Console.ReadLine();
+}
+Console.WriteLine();
+
+Array.Sort(zipCodes);
+
+Console.WriteLine("Sorted zip codes:");
+foreach (string z in zipCodes)
+{
+    Console.WriteLine(z);
+}
